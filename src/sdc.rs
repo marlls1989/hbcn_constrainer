68d02f1ad878d64ef975fd21543095f0bae48437
Marcos Sartori <marcos.sartori@acad.pucrs.br>
2022-06-06 18:24:54 -0300

    constraining inputs and outputs on alho

--- src/sdc.rs
+++ src/sdc.rs
@@ -21,12 +21,32 @@ fn port_wildcard(s: &str) -> String {
     }
 }
 
+fn port_instance(s: &str) -> String {
+    lazy_static! {
+        static ref REPLACE_RE: Regex = Regex::new(r"^port:([^/]+)/(.+)").unwrap();
+        static ref INDEX_RE: Regex = Regex::new(r"^(.+)\[([0-9]+)\]").unwrap();
+    }
+
+    let s = if let Some(c) = REPLACE_RE.captures(s) {
+        format!("inst:{}/i{}", &c[1], &c[2])
+    } else {
+        format!("inst:{}", s)
+    };
+
+    if let Some(c) = INDEX_RE.captures(&s) {
+        format!("{}_{}/*", &c[1], &c[2])
+    } else {
+        format!("{}/*", s)
+    }
+}
+
 fn dst_rails(s: &CircuitNode) -> String {
     match s {
         CircuitNode::Port(name) => {
             format!(
-                "[get_ports [vfind {{{}}}] -filter {{direction == out}}]",
-                port_wildcard(name)
+                "[list [get_ports [vfind {{{}}}] -filter {{direction == out}}] [get_pins -of_objects [get_cells [vfind {{{}}}] -filter {{is_sequential == true}}] -filter {{direction == in}}]]",
+                port_wildcard(name),
+                port_instance(name),
             )
         }
         CircuitNode::Register { name, .. } => format!(
